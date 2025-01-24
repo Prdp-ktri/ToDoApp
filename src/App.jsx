d067f5af18061be78a8c7514962595e959643c3c
Pradeep Khatri <pradeepkhatri44@live.com>
2025-01-24 18:22:33 +0530

    30 Simple todos app created
    30 Simple todos app created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-
+import { useEffect, useState } from "react";
+import classes from "./styles.module.css";
+import TodoItem from "./components/todo-item";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [todoList, setTodoList] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  async function fetchListOfTodos() {
+    try {
+      setLoading(true);
+      const apiResponse = await fetch("https://dummyjson.com/todos");
+      const result = await apiResponse.json();
+      console.log(result);
+      if (result?.todos && result?.todos?.length > 0) {
+        setTodoList(result?.todos);
+        setLoading(false);
+        setErrorMsg("");
+      } else {
+        setTodoList([]);
+        setLoading(false);
+        setErrorMsg("");
+      }
+    } catch (e) {
+      console.log(e);
+      setErrorMsg("Some error occurred");
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfTodos();
+  }, []);
 
   return (
-    <div>
-      <h1>Simple ToDo App Using Material UI</h1>
+    <div className={classes.mainWrapper}>
+      <h1 className={classes.headerTitle}>Simple ToDo App Using Material UI</h1>
+      <div>
+        {todoList && todoList.length > 0
+          ? todoList.map((todo) => <TodoItem key={todo.id} />)
+          : null}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/todo-item/Index.jsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
+function TodoItem({ todo }) {
+  console.log(todo);
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h5" color={"text.secondary"}>
+          {todo?.todo}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default TodoItem;
